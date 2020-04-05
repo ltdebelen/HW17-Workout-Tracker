7729534edcc7f4ec42397561e26f462063c3e521
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-05 17:09:53 -0500

    Seed inserted. Adding morgan on server.js for logging

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,9 +9,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(logger("dev"));
 
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}!`);
