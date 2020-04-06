4d7f35d564d2349f54dc03ad376e6095d3af1b11
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-05 20:06:46 -0500

    Adding API routes code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,39 @@
 const path = require("path");
+const Workout = require("../models/Workout");
 
 module.exports = (app) => {
   app.get("/api/workouts", (req, res) => {
-    res.json(200);
+    Workout.find({})
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => res.status(400).json(err));
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => res.status(400).json(err));
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => res.status(400).json(err));
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+      { id: req.params.id },
+      { $push: { exercises: req.body } }
+    )
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => res.status(400).json(err));
   });
 };
