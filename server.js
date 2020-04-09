6902f2e884a980b72026604ed2bd35e6454f47d1
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-08 21:09:59 -0500

    Updating mongodb connection string

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ app.use(logger("dev"));
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:p@$$w0rd@ds335668.mlab.com:35668/heroku_9p5vfzzz",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}!`);
